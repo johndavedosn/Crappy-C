23150e6baaf69e8a0059a748cda5606043969dfb
johndavedosn <112868391+johndavedosn@users.noreply.github.com>
2024-01-14 21:33:06 +0100

    Add files via upload

---
+++ sms.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define null NULL
+struct Class {
+    int classid;
+    int stdnum;
+    float average_grade;
+};
+struct Teacher {
+    char name[24];
+    char subject[24];
+    struct Class* class;
+};
+struct Student {
+    int sid;
+    struct Class* class;
+    struct Teacher* teacher;
+    char name[24];
+    float latest_grade;
+};
+struct Class* AddClass(int id, int num){
+    struct Class* class = (struct Class*)malloc(sizeof(struct Class));
+    if (class == null){
+        return null;
+    }
+    class->classid = id;
+    class->stdnum = num; 
+    return class;
+};
+struct Teacher* AddTeacher(char name[24], char subject[24], struct Class* cls){
+    struct Teacher* tchr = (struct Teacher*)malloc(sizeof(struct Teacher));
+    if (tchr == null){
+        return null;
+    }
+    strcpy(tchr->name, name);
+    strcpy(tchr->subject, subject);
+    tchr->class = cls;
+    return tchr;
+};
+struct Student* AddStudent(int sid, struct Class* cls, struct Teacher* tchr, char name[24]){
+    struct Student* stdnt = (struct Student*)malloc(sizeof(struct Student));
+    if (stdnt == null){
+        return null;
+    }
+    stdnt->sid = sid;
+    stdnt->class = cls;
+    stdnt->teacher= tchr;
+    strcpy(stdnt->name, name);
+    return stdnt;
+
+};
+
+int DeleteClass(struct Class* cls){
+    if (cls == NULL){
+        return -1;
+    }
+    free(cls);
+    return 0;
+}
+int DeleteTeacher(struct Teacher* tchr){
+    if (tchr == null){
+        return null;
+    }
+    free(tchr);
+    return 0;
+}
+int DeleteStudent(struct Student* stdnt){
+    if (stdnt == null){
+        return null;
+    }
+    free(stdnt);
+    return 0;
+}
+
+
+int main() {
+    
+    return 0;
+}
